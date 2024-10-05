bef3f7a38d07cfda81f475d27862688346f3163d
Aviel Aldama <aviel.aldama@gmail.com>
2024-10-05 12:16:10 -0600

    heigt input search

--- app/components/headertop/index.tsx
+++ app/components/headertop/index.tsx
@@ -95,7 +95,7 @@ const Index = (props: { routes: Array<{ route: string, nameroute: string, icon: 
                                     <i className="bi bi-search"></i>
                                 </div>
                                 <input
-                                    className="w-full pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input"
+                                    className="w-full h-8 pl-8 pr-2 text-sm text-gray-700 placeholder-gray-600 bg-gray-100 border-0 rounded-md dark:placeholder-gray-500 dark:focus:shadow-outline-gray dark:focus:placeholder-gray-600 dark:bg-gray-700 dark:text-gray-200 focus:placeholder-gray-500 focus:bg-white focus:border-purple-300 focus:outline-none focus:shadow-outline-purple form-input"
                                     type="text"
                                     placeholder="Search for projects"
                                 />
