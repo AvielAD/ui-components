855fd22528eb5c7e3d9e005ba0815400ce445931
Aviel Aldama <aviel.aldama@gmail.com>
2025-09-19 01:11:15 -0600

    remover dark

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -3,10 +3,10 @@ const Index = (props: { close: ()=>void,show: boolean, children: string | JSX.El
     return (
         <div
         onClick={()=>props.close()} 
-        className={`${props.show ? "fixed":"hidden"} inset-0 z-30 flex bg-black bg-opacity-50 items-center justify-center`}>
+        className={`${props.show ? "fixed":"hidden"} inset-0 z-30 flex bg-black opacity-50 items-center justify-center`}>
             <div 
             onClick={(e)=> e.stopPropagation()}
-            className="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg dark:bg-secondary-800 rounded-lg m-4 max-w-xl">
+            className="w-full px-6 py-4 overflow-hidden bg-white rounded-t-lg">
                 <div className="">
                     {props.children}
                 </div>
